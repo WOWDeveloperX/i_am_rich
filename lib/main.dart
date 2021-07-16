dbbf8ad6f96d0551d0486ce411df5cd2ef4be6b2
WOWDeveloperX <wowdeveloperx@gmail.com>
2021-07-16 20:20:51 +0300

    Revert "2"
    This reverts commit 22e69833ea55c2a989b7563fd713c9dd89268706.

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Helloy world'),
+      ),
+    ),
+  );
+}
+
+<<<<<<< HEAD
+=======
+
+
+>>>>>>> parent of 22e6983 (2)
