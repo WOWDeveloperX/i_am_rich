8298f11166155b13129ba9f6fd0a1abddff561ef
WOWDeveloperX <wowdeveloperx@gmail.com>
2021-07-17 20:20:33 +0300

    imageNaruto

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Helloy world'),
+      home: Scaffold(
+        backgroundColor: Colors.orange[100],
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text("I Am Rich"),
+        ),
+        body: Center(
+          child: Image(
+              image: NetworkImage(
+                  'https://i.pinimg.com/originals/36/e1/96/36e1965a5c2dce072d92a143cc87d517.jpg')),
+        ),
       ),
     ),
   );
 }
-
