09be689afe1aa75a13a6dbe74a9d71f758831240
WOWDeveloperX <wowdeveloperx@gmail.com>
2021-07-20 20:29:35 +0300

    NotIcon

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,16 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.orange[100],
+        backgroundColor: Colors.blueGrey,
         appBar: AppBar(
           backgroundColor: Colors.blueGrey[900],
           title: Text("I Am Rich"),
         ),
         body: Center(
           child: Image(
-              image: NetworkImage(
-                  'https://i.pinimg.com/originals/36/e1/96/36e1965a5c2dce072d92a143cc87d517.jpg')),
+              image: AssetImage(
+                  'images/diamond.png')
+                  ),
         ),
       ),
     ),
